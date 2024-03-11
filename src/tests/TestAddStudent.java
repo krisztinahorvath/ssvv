fe3df74405a2754fc79058402f0d57a8ca29d3a5
krisztinahorvath <91715582+krisztinahorvath@users.noreply.github.com>
2024-03-11 18:58:58 +0200

    added project v2

---
+++ src/tests/TestAddStudent.java
@@ -0,0 +1,46 @@
+package tests;
+
+import domain.Student;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import repository.NotaXMLRepo;
+import repository.StudentXMLRepo;
+import repository.TemaXMLRepo;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.ValidationException;
+
+import static org.junit.Assert.assertThrows;
+
+
+public class TestAddStudent {
+    @Test
+    public void testAddStudent(){
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+        // invalid student
+        Student student1 = new Student("", "ana", 933, "ana@email.com");
+
+        assertThrows(ValidationException.class, () -> {
+            service.addStudent(student1);
+        });
+
+        // valid student
+        Student student2 = new Student("12345", "maria", 933, "maria@email.com");
+
+        Assertions.assertNull(service.addStudent(student2));
+
+    }
+}
