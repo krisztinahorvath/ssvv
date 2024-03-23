db81eea863479622a3c683799460c2368428c756
krisztinahorvath <91715582+krisztinahorvath@users.noreply.github.com>
2024-03-23 23:59:50 +0200

    added test cases

--- src/main/java/validation/StudentValidator.java
+++ src/main/java/validation/StudentValidator.java
@@ -1,6 +1,9 @@
 package validation;
 
 import domain.Student;
+import repository.StudentXMLRepo;
+
+import java.util.Objects;
 
 public class StudentValidator implements Validator<Student> {
 
@@ -11,26 +14,39 @@ public class StudentValidator implements Validator<Student> {
      */
     @Override
     public void validate(Student entity) throws ValidationException {
+        if(entity.getID() == null){ // bug fix 1, moved if up
+            throw new ValidationException("Id incorect!");
+        }
+
         if(entity.getID().equals("")){
             throw new ValidationException("Id incorect!");
         }
-        if(entity.getID() == null){
-            throw new ValidationException("Id incorect!");
-        }
+
         if(entity.getNume() == ""){
             throw new ValidationException("Nume incorect!");
         }
-        if(entity.getGrupa() < 0) {
+
+        if(entity.getGrupa() <= 0) { // bug fix 2, before it was < 0
             throw new ValidationException("Grupa incorecta!");
         }
+
         if(entity.getEmail() == null){
             throw new ValidationException("Email incorect!");
         }
+
         if(entity.getNume() == null){
             throw new ValidationException("Nume incorect!");
         }
+
         if(entity.getEmail().equals("")){
             throw new ValidationException("Email incorect!");
+        }
+    }
+
+    public void validateEmail(StudentXMLRepo studentFileRepository, String email){
+        for(Student student: studentFileRepository.findAll()){
+            if(Objects.equals(student.getEmail(), email))
+                throw new ValidationException("Email is not unque!");
         }
     }
 }
--- src/test/java/ssvv/TestAddStudent.java
+++ src/test/java/ssvv/TestAddStudent.java
@@ -1,8 +1,10 @@
 package ssvv;
 
 import domain.Student;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 import repository.NotaXMLRepo;
 import repository.StudentXMLRepo;
 import repository.TemaXMLRepo;
@@ -10,7 +12,15 @@ import service.Service;
 import validation.NotaValidator;
 import validation.StudentValidator;
 import validation.TemaValidator;
+import validation.ValidationException;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+// TODO fix addStudent bug if it exists
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestAddStudent{
     public static Service service;
 
@@ -18,9 +28,9 @@ public class TestAddStudent{
     public static void setup(){
         StudentValidator studentValidator = new StudentValidator();
         TemaValidator temaValidator = new TemaValidator();
-        String filenameStudent = "fisiere/Studenti.xml";
-        String filenameTema = "fisiere/Teme.xml";
-        String filenameNota = "fisiere/Note.xml";
+        String filenameStudent = "fisiere/TestStudenti.xml";
+        String filenameTema = "fisiere/TestTeme.xml";
+        String filenameNota = "fisiere/TestNote.xml";
 
         StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
         TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
@@ -29,19 +39,96 @@ public class TestAddStudent{
         service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
     }
 
-    @Test
-    public void addStudentWorks(){
-        // valid student
-        Student student2 = new Student("1234567", "maria", 933, "maria@email.com");
+    @AfterAll
+    public static void tearDown() {
+        String filenameStudent = "fisiere/TestStudenti.xml";
+        String filenameTema = "fisiere/TestTeme.xml";
+        String filenameNota = "fisiere/TestNote.xml";
 
-        service.addStudent(student2);
+        try {
+            Files.write(Paths.get(filenameStudent), "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><inbox> </inbox>".getBytes());
+            Files.write(Paths.get(filenameTema), "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><inbox> </inbox>".getBytes());
+            Files.write(Paths.get(filenameNota), "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><inbox> </inbox>".getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    public void addStudentFails(){
-        Student student1 = new Student("", "ana", 933, "ana@email.com");
+    @Order(1)
+    public void addStudentWorks() {
+        Student student = new Student("20000", "Sharon", 933, "sharon@email.com");
+        assertNull(service.addStudent(student));
+    }
 
+    @Test
+    @Order(2)
+    public void addStudentNonUniqueId(){
+        Student student = new Student("20000", "Sharon", 933, "sharon1@email.com");
+        Student retStudent = service.addStudent(student);
+        assertEquals(retStudent.getID(), "20000");
+    }
 
-        service.addStudent(student1);
+    @Test
+    @Order(3)
+    public void addStudentNullId(){
+        Student student = new Student(null, "Sharon", 933, "sharon1@email.com");
+        assertThrows(ValidationException.class, () -> service.addStudent(student));
+    }
+
+    @Test
+    @Order(4)
+    public void addStudentEmptyId(){
+        Student student = new Student("", "Sharon", 933, "sharon1@email.com");
+        assertThrows(ValidationException.class, () -> service.addStudent(student));
+    }
+
+    @Test
+    @Order(5)
+    public void addStudentNullName() {
+        Student student = new Student("20001", null, 933, "sharon1@email.com");
+        assertThrows(ValidationException.class, () -> service.addStudent(student));
+    }
+
+    @Test
+    @Order(6)
+    public void addStudentEmptyName() {
+        Student student = new Student("20001", "", 933, "sharon1@email.com");
+        assertThrows(ValidationException.class, () -> service.addStudent(student));
+    }
+
+    @Test
+    @Order(7)
+    public void addStudentNegativeGroupNo() {
+        Student student = new Student("20001", "Sharon", -933, "sharon1@email.com");
+        assertThrows(ValidationException.class, () -> service.addStudent(student));
+    }
+
+    @Test
+    @Order(8)
+    public void addStudentEmptyEmail() {
+        Student student = new Student("20001", "Sharon", 933, "");
+        assertThrows(ValidationException.class, () -> service.addStudent(student));
+    }
+
+    @Test
+    @Order(9)
+    public void addStudentNullEmail() {
+        Student student = new Student("20001", "Sharon", 933, null);
+        assertThrows(ValidationException.class, () -> service.addStudent(student));
+    }
+
+    @Test
+    @Order(10)
+    public void addStudentDuplicateEmail() {
+        Student student = new Student("20001", "Sharon", 933, "sharon@email.com");
+        assertThrows(ValidationException.class, () -> service.addStudent(student));
+    }
+
+    @Test
+    @Order(11)
+    public void addStudentNotStrictlyPositiveGroupNo() {
+        Student student = new Student("20001", "Sharon", 0, "sharon@email.com");
+        assertThrows(ValidationException.class, () -> service.addStudent(student));
     }
 }
